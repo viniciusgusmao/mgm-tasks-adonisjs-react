22952c18c49ffb58991762210b4aa1744b8166ca
Vinicius Gusmao <vinicius-og@hotmail.com>
2020-05-02 20:54:30 -0300

    Opção de excluir iniciada - WIP

--- frontend/src/pages/BaseForm.js
+++ frontend/src/pages/BaseForm.js
@@ -1,75 +1,101 @@
-import React,{ useState } from 'react';
-import Notification from 'components/Notification';
-import Loading from 'components/Loading';
-import { useHistory } from 'react-router-dom'
+import React, { useState } from "react";
+import Notification from "components/Notification";
+import Loading from "components/Loading";
+import { useHistory } from "react-router-dom";
 
-import { store, update } from 'services/crud';
+import { store, update, destroy } from "services/crud";
 
-const BaseForm = ({children}) => {
+const BaseForm = ({ children }) => {
   const history = useHistory();
-  const [errorApiRequest, setErrorApiRequest] = useState([])
+  const [errorApiRequest, setErrorApiRequest] = useState([]);
   const [loading, setLoading] = useState(false);
   const [toast, setToast] = useState(false);
-  const [paramsToast, setParamsToast] = useState({ description: '', type: '' });
+  const [paramsToast, setParamsToast] = useState({ description: "", type: "" });
 
   const execToast = (isToasty, description, type, currentPath) => {
-    setToast(isToasty)
-    setParamsToast({ description, type }) 
+    setToast(isToasty);
+    setParamsToast({ description, type });
     setTimeout(() => {
-      history.push(currentPath)
-    }, 2000)
-  }
+      history.push(currentPath);
+    }, 2000);
+  };
 
-  const storeAction = (currentPath,values) => {
-    setLoading(true)
-    setErrorApiRequest([])
-    store(currentPath,values)
+  const storeAction = (currentPath, values) => {
+    setLoading(true);
+    setErrorApiRequest([]);
+    store(currentPath, values)
       .then((response) => {
         const { data } = response;
-        if (data.hasOwnProperty('length')){
-          let errors = data.map((item,idx) => item[idx] = item.message).join('<br>');
+        if (data.hasOwnProperty("length")) {
+          let errors = data
+            .map((item, idx) => (item[idx] = item.message))
+            .join("<br>");
           setErrorApiRequest(errors);
         } else
-          execToast(true,"Cadastro realizado com sucesso",'success',currentPath)
-        
-        setLoading(false)
+          execToast(
+            true,
+            "Cadastro realizado com sucesso",
+            "success",
+            currentPath
+          );
+
+        setLoading(false);
       })
       .catch((error) => {
-        execToast(true,"Sua sessão expirou",'fail','/login')
-        setLoading(false)
-      })
-  }
+        execToast(true, "Sua sessão expirou", "fail", "/login");
+        setLoading(false);
+      });
+  };
 
-  const updateAction = (currentPath,id,values) => {
-    setLoading(true)
-    update(currentPath,id,values)
+  const updateAction = (currentPath, id, values) => {
+    setLoading(true);
+    update(currentPath, id, values)
       .then((response) => {
         const { data } = response;
-        if (data.hasOwnProperty('length')){
-          let errors = data.map((item,idx) => item[idx] = item.message).join('<br>');
+        if (data.hasOwnProperty("length")) {
+          let errors = data
+            .map((item, idx) => (item[idx] = item.message))
+            .join("<br>");
           setErrorApiRequest(errors);
         } else
-          execToast(true,"Atualização realizada com sucesso",'success',currentPath)
-        
-        setLoading(false)
+          execToast(
+            true,
+            "Atualização realizada com sucesso",
+            "success",
+            currentPath
+          );
+
+        setLoading(false);
       })
       .catch((error) => {
-        execToast(true,"Sua sessão expirou",'fail','/login')
-        setLoading(false)
-      })
-  }
+        execToast(true, "Sua sessão expirou", "fail", "/login");
+        setLoading(false);
+      });
+  };
+
+  const deleteAction = (currentPath, id, values) => {
+    setLoading(true);
+    destroy(currentPath, id, values)
+      .then((response) => {})
+      .catch((error) => {});
+  };
 
   return (
     <>
-       {toast && <Notification message={paramsToast.description} type={paramsToast.type} />}
-       <Loading loading={loading} />
-       {children(
-          (currentPath,values) => storeAction(currentPath,values),
-          (currentPath,id,values) => updateAction(currentPath,id,values),
-          errorApiRequest
-          )}
+      {toast && (
+        <Notification
+          message={paramsToast.description}
+          type={paramsToast.type}
+        />
+      )}
+      <Loading loading={loading} />
+      {children(
+        (currentPath, values) => storeAction(currentPath, values),
+        (currentPath, id, values) => updateAction(currentPath, id, values),
+        errorApiRequest
+      )}
     </>
   );
-}
+};
 
 export default BaseForm;
--- frontend/src/services/crud.js
+++ frontend/src/services/crud.js
@@ -1,10 +1,14 @@
-import api from 'config/api';
-import urls from 'res/urls';
+import api from "config/api";
+import urls from "res/urls";
 
-export const store = (path,data) => {
-  return api.post(`${urls.api+''+path}`, data);
-}
+export const store = (path, data) => {
+  return api.post(`${urls.api + "" + path}`, data);
+};
 
-export const update = (path,id,data) => {
-  return api.put(`${urls.api+''+path+'/'+id}`, data);
-}
+export const update = (path, id, data) => {
+  return api.put(`${urls.api + "" + path + "/" + id}`, data);
+};
+
+export const destroy = (path, id, data) => {
+  return api.delete(`${urls.api + "" + path + "/" + id}`, data);
+};
